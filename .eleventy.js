06be0a0935525fd81bb84df826a0c46ab7543f7d
VirajS00 <virajshukla00@gmail.com>
2021-11-06 02:29:03 +0530

    added prism

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
+const codeStyleHooks = require('eleventy-plugin-code-style-hooks');
+
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addWatchTarget('./src/sass/');
 	eleventyConfig.addPassthroughCopy('./src/js');
+	eleventyConfig.addPlugin(codeStyleHooks);
 
 	eleventyConfig.setFrontMatterParsingOptions({
 		excerpt: true,
--- src/articles/page2.md
+++ src/articles/page2.md
@@ -8,6 +8,6 @@ this is page 2.
 
 loremwnnwnnwnnwnn
 
-```c++
+```cpp
 cout << "hello world";
 ```
